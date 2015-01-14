e3bfeeb9c9007a088bcd8bbf2e5dfdd29fe86a06
Renato Camilio <renatomcamilio@gmail.com>
2015-01-14 14:04:52 -0800

    Initial Commit

---
+++ LinkedLists/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LinkedLists
+//
+//  Created by Renato Camilio on 1/14/15.
+//  Copyright (c) 2015 Renato Camilio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
